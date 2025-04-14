9a95964d8b4029163ad89f278d451dd3c25ce20e
Fatwaakbarjiwani <fatwaakbar2003@gmail.com>
2025-04-14 12:52:07 +0700

    update

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers";
 export default configureStore({
   reducer: rootReducer,
-  devTools: import.meta.env.VITE_MODE !== "production",
-  // devTools: false,
+  // devTools: import.meta.env.VITE_MODE !== "production",
+  devTools: false,
 });
